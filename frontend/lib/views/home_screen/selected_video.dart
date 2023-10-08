86c72d2fa832a6c9b3cf2557788904839ce82901
Mahakaal <59462639+O-ozwal@users.noreply.github.com>
2023-10-08 23:55:57 +0545

    text editor

--- frontend/lib/views/home_screen/selected_video.dart
+++ frontend/lib/views/home_screen/selected_video.dart
@@ -1,12 +1,17 @@
 import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
+import 'package:tapioca/tapioca.dart';
+import 'package:flutter/services.dart';
+
 import 'package:pro_shorts/constants.dart';
 import 'package:pro_shorts/get/home_screen/get_select_video.dart';
+import 'package:pro_shorts/views/editor/editor_screen.dart';
 import 'package:pro_shorts/views/home_screen/upload_video.dart';
-import 'package:video_player/video_player.dart';
 
 class SelectedVideo extends StatefulWidget {
   File videoPath;
@@ -20,11 +25,110 @@ class SelectedVideo extends StatefulWidget {
 
 class _SelectedVideoState extends State<SelectedVideo> {
   late SelectVideoController selectVideoController;
+  final TextEditingController _textEditingController = TextEditingController();
+  bool isTextFieldVisible = false;
+  int processPercentage = 0;
+
+  String textOverVideo = '';
+  static const EventChannel _channel =
+      const EventChannel('video_editor_progress');
+  late StreamSubscription _streamSubscription;
   @override
   void initState() {
     super.initState();
     selectVideoController = Get.put(SelectVideoController());
     selectVideoController.initializeVideo(widget.videoPath);
+  }
+
+  void _enableEventReceiver() {
+    _streamSubscription =
+        _channel.receiveBroadcastStream().listen((dynamic event) {
+      setState(() {
+        processPercentage = (event.toDouble() * 100).round();
+      });
+    }, onError: (dynamic error) {
+      print('Received error: ${error.message}');
+    }, cancelOnError: true);
+  }
+
+  void _disableEventReceiver() {
+    _streamSubscription.cancel();
+  }
+
+  void _toggleTextField(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                textOverVideo =
+                                    _textEditingController.text.trim();
+                                final tapiocaBalls = [
+                                  TapiocaBall.textOverlay(textOverVideo, 100,
+                                      10, 100, Colors.amber),
+                                ];
+                                final cup = Cup(
+                                    Content(widget.videoPath.toString()),
+                                    tapiocaBalls);
+                                cup.suckUp('/storage/emulated/0').then(
+                                  (_) async {
+                                    print("finished");
+                                    setState(
+                                      () {
+                                        processPercentage = 0;
+                                      },
+                                    );
+                                  },
+                                );
+                              },
+                              icon: const Icon(Icons.send))),
+                      autofocus: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+        // context: context,
+        // builder: (BuildContext context) {
+        //   return Column(
+        //     mainAxisSize: MainAxisSize.min,
+        //     children: <Widget>[
+        //       Padding(
+        //         padding: EdgeInsets.all(16.0),
+        //         child: Expanded(
+        //           child: TextField(
+        //             controller: _textEditingController,
+        //             onChanged: (text) {
+        //               setState(() {
+        //                 textOverVideo = _textEditingController.text
+        //                     .trim(); // Store the input text
+        //               });
+        //             },
+        //             decoration: InputDecoration(
+        //               hintText: 'Enter text...',
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ],
+        //   );
+        // },
+        );
   }
 
   @override
@@ -92,10 +196,17 @@ class _SelectedVideoState extends State<SelectedVideo> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
-                      children: const [
-                        Icon(
-                          Icons.edit,
-                          color: white,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Get.to(
+                              () => EditorScreen(file: widget.videoPath),
+                            );
+                          },
+                          child: Icon(
+                            Icons.edit,
+                            color: white,
+                          ),
                         ),
                         SizedBox(
                           height: 5,
@@ -108,11 +219,18 @@ class _SelectedVideoState extends State<SelectedVideo> {
                       ],
                     ),
                     Column(
-                      children: const [
-                        Text(
-                          "Aa",
-                          style: TextStyle(
-                              color: white, fontWeight: FontWeight.bold),
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            _toggleTextField(context);
+                          },
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<OutlinedBorder>(
+                                  StadiumBorder())),
+                          child: Icon(
+                            Icons.text_fields,
+                            color: Colors.red,
+                          ),
                         ),
                         SizedBox(
                           height: 5,
@@ -125,7 +243,7 @@ class _SelectedVideoState extends State<SelectedVideo> {
                       ],
                     ),
                     Column(
-                      children: [
+                      children: const [
                         Icon(
                           FontAwesomeIcons.noteSticky,
                           color: white,
--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import ffmpeg_kit_flutter_min
 import firebase_auth
 import firebase_core
 import firebase_storage
@@ -15,6 +16,7 @@ import video_compress
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
